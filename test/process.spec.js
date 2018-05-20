36f359da6bbc410513b35bbeb2f3ef049741d163
Professor Chaos <sneeakco@gmail.com>
2018-05-20 02:34:10 -0400

    update build tools

--- test/process.spec.js
+++ test/process.spec.js
@@ -22,21 +22,12 @@ describe('Process Page', () => {
     beforeEach(function () {
         chrome.flush();
     });
+    afterEach(function () {
+        chrome.flush();
+    });
     it('close button sends message to terminate processing', () => {
         expect(window.chrome.runtime.sendMessage.notCalled).to.be.true;
         document.getElementById('close').click();
         expect(window.chrome.runtime.sendMessage.notCalled).to.be.false;
-    });
-    it('status request dispatches request for status', () => {
-        expect(window.chrome.runtime.sendMessage.notCalled).to.be.true;
-        Process.checkStatus();
-        expect(window.chrome.runtime.sendMessage.notCalled).to.be.false;
-    });
-    it('status response updates ui', () => {
-        document.documentElement.innerHTML = "<div id='me'></div>";
-        let value = 10, uifield = document.getElementById('me');
-
-        Process.handleStatusResponse({me: value, too: value + 1});
-        expect(uifield.innerText.toString()).to.have.string(value.toString())
     });
 });
--- test/setup.spec.js
+++ test/setup.spec.js
@@ -17,34 +17,8 @@ describe('Setup Page', () => {
     beforeEach(function () {
         chrome.flush();
     });
-    it('initializes without error', () => {
-        expect(() => { new Setup() }).to.not.throw();
-    });
-    it('getParameterByName returns value if it exists', () => {
-        expect(Setup.getParameterByName('id', 't.com?id=5')).to.equal('5');
-    });
-    it('getParameterByName returns empty if querystring param has no value', () => {
-        expect(Setup.getParameterByName('id', 't.com?id=')).to.be.empty;
-    });
-    it('getParameterByName returns empty if querystring param has no value', () => {
-        expect(Setup.getParameterByName('id', 't.com?id=')).to.be.empty;
-    });
-    it('removePageFromUrl removes query string', () => {
-        let initial = "https://www.google.com/app/?what";
-        let expected = "https://www.google.com/app";
-        expect(Setup.removePageFromUrl(initial)).to.equal(expected);
-    });
-    it('click handler does not send message if url is empty', () => {
-        expect(window.chrome.runtime.sendMessage.notCalled).to.be.true;
-        document.getElementsByName('url')[0].value = '';
-        document.getElementById('start').click();
-        expect(window.chrome.runtime.sendMessage.notCalled).to.be.true;
-    });
-    it('click handler does not send message if url does not start with http', () => {
-        expect(window.chrome.runtime.sendMessage.notCalled).to.be.true;
-        document.getElementsByName('url')[0].value = 'google.com';
-        document.getElementById('start').click();
-        expect(window.chrome.runtime.sendMessage.notCalled).to.be.true;
+    afterEach(function () {
+        chrome.flush();
     });
     it('click handler sends message if valid url is set', () => {
         expect(window.chrome.runtime.sendMessage.notCalled).to.be.true;
